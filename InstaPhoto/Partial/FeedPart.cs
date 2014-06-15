686ad885b979f8071e0e959e9a50f039b0c47ce2
zikkryp <zikkryp@gmail.com>
2014-06-15 20:53:05 +0300

    partial upload

---
+++ InstaPhoto/Partial/FeedPart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls.Primitives;
+using Bing.Maps;
+
+namespace InstaPhoto
+{
+    public sealed partial class FeedPage
+    {
+        Menu menu;
+        Popup popup;
+
+        Pushpin p;
+        Storage storage;
+        public static User CurrentUser;
+        User user;
+        Feed feedItem;
+    }
+}
